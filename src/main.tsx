53613372c1e7b91c11f9beaabd4441de887734f9
Tanu <144295793+CupOfMakiato@users.noreply.github.com>
2024-06-09 21:57:21 +0700

    deploy bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/vite-project/",
+    element: <App />,
+    children: [
+
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
